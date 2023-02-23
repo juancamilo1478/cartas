06a9e1ef216c9f9032fbf8aa9439bd9c286e5a25
juancamilo1478 <juancamilo1478@gmail.com>
2023-02-23 01:05:21 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Routes,Route, useLocation  } from 'react-router-dom'
 import  Portada from "./components/portadas/Portada"
 import Cartas from "./components/cards/Cards"
 import SearchBar from './components/searchBar/SearchBar'
-
+import Favorites from './components/favorites/favorites'
+import Detail from './components/cards/Detail'
 function App() {
   const lugar=useLocation()
 
@@ -11,12 +12,14 @@ function App() {
     return(<Routes>
       <Route path='/' element={<Portada/>} />
       <Route path='/cartas' element={<Cartas/>}/>
+       <Route path='/detail/:detailId'  element={<Detail/>}/>
     </Routes>)
   }else{return(<div>
     <SearchBar/>
     <Routes>
-     
+      <Route path='/favorites' element={<Favorites/>} />
       <Route path='/cartas' element={<Cartas/>}/>
+       <Route path='/detail/:detailId'  element={<Detail/>}/>
     </Routes>
   </div>)}
 
---
+++ src/components/cards/Detail.jsx
@@ -0,0 +1,58 @@
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import estilo from "./Detail.module.css";
+import fondo from "../../img/fondo.jpg";
+import portal from "../../img/portal.png";
+
+function Detail() {
+  const [character, setCharacter] = useState("");
+  let { detailId } = useParams();
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${detailId}`)
+      .then((response) => response.json())
+      .then((char) => {
+        if (char.name) {
+          setCharacter(char);
+        } else {
+          window.alert("No hay personajes con ese ID");
+        }
+      })
+      .catch((err) => {
+        window.alert("No hay personajes con ese ID");
+      });
+    return setCharacter({});
+  }, [detailId]);
+
+  return (
+    <div>
+      <img src={fondo} alt="foto" className={estilo.foto} />
+      <dic className={estilo.contenedor}>
+        <div className={estilo.texto}>
+          <Link to="/cartas" className={estilo.lin}>
+            <div className={estilo.botton}>
+              <img src={portal} alt="fotso" className={estilo.portal} />
+              <div className={estilo.circulo}></div>
+              <p>Salir</p>
+            </div>
+          </Link>
+
+          <h1 className={estilo.letras}>Nombre: {character?.name}</h1>
+          <h2 className={estilo.letras}>Especie: {character?.species}</h2>
+          <h2 className={estilo.letras}>Genero: {character?.gender}</h2>
+          <h2 className={estilo.letras}>Estado: {character?.status}</h2>
+          <h2 className={estilo.letras}>lugar: {character?.origin?.name}</h2>
+        </div>
+
+        <div className={estilo.imagen}>
+          <img
+            src={character?.image}
+            alt={character?.name}
+            className={estilo.figura}
+          />
+        </div>
+      </dic>
+    </div>
+  );
+}
+export default Detail;
