52fb1a1d2321538bb7316c2dd71a06c60ca9385a
juancamilo1478 <juancamilo1478@gmail.com>
2023-02-23 00:36:28 -0500

    4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 
-import { Routes,Route  } from 'react-router-dom'
+import { Routes,Route, useLocation  } from 'react-router-dom'
 import  Portada from "./components/portadas/Portada"
 import Cartas from "./components/cards/Cards"
+import SearchBar from './components/searchBar/SearchBar'
+
 function App() {
-  return (<div>
-    <Routes>
+  const lugar=useLocation()
+
+  if(lugar.pathname==='/' ){
+    return(<Routes>
       <Route path='/' element={<Portada/>} />
       <Route path='/cartas' element={<Cartas/>}/>
+    </Routes>)
+  }else{return(<div>
+    <SearchBar/>
+    <Routes>
+     
+      <Route path='/cartas' element={<Cartas/>}/>
     </Routes>
+  </div>)}
 
-  </div>
-  )
 }
 
 export default App;
